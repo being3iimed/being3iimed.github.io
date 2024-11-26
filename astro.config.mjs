1d59bd379e762318d19c2745edc2d5234f781e1d
Imed Kouki <88271652+being3iimed@users.noreply.github.com>
2024-11-26 19:18:10 +0000

    Updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://being3iimed.github.io',
+  base: 'being3iimed.github.io',
+})
